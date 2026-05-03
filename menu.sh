7cc5f3fad14c9925bdf5e6f5824ee53e847dfeb9
sallycarred-ai <sallycarred@gmail.com>
2026-05-03 12:20:55 +1000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,55 +1,60 @@
 #!/bin/bash
-while true
-do
-  clear
-  toilet -f mono12 -F border "CYBERDECK SAL"
-  echo ""
-  echo ""
-  echo "$(date +%H:%M)"
-  echo "==========================="
+
 OPTIONS=("Notes" "Photos" "Videos" "System" "Files" "Quick Note")
 SELECTED=0
 
-while true; do
+draw_menu() {
     clear
 
-    echo "==== CYBERDECK SAL ===="
-    echo "$(date +%H:%M)"
-    echo "Use ↑ ↓ and ENTER"
+    # BIG CYBERDECK TITLE
+    toilet -f mono12 -F border "CYBERDECK SAL"
+
+    echo ""
+    printf "      %s\n" "$(date +%H:%M)"
+    printf "  Use ↑ ↓ and ENTER\n"
     echo ""
 
     for i in "${!OPTIONS[@]}"; do
         if [ $i -eq $SELECTED ]; then
-            echo "> ${OPTIONS[$i]}"
+            printf "   > %s\n" "${OPTIONS[$i]}"
         else
-            echo "  ${OPTIONS[$i]}"
+            printf "     %s\n" "${OPTIONS[$i]}"
         fi
     done
+
+    echo ""
+}
+
+while true
+do
+    draw_menu
 
     read -rsn1 key
 
     if [[ $key == $'\x1b' ]]; then
         read -rsn2 key
-
         case $key in
             "[A") ((SELECTED--)) ;;  # UP
             "[B") ((SELECTED++)) ;;  # DOWN
         esac
+
     elif [[ $key == "" ]]; then
+        clear
         case $SELECTED in
             0) nano Notes/notes.txt ;;
-            1) echo "Photos coming soon"; sleep 1 ;;
-            2) echo "Videos coming soon"; sleep 1 ;;
+            1) echo "📸 Photos coming soon"; sleep 1 ;;
+            2) echo "🎬 Videos coming soon"; sleep 1 ;;
             3) htop ;;
             4) mc ;;
             5) nano Notes/quick.txt ;;
         esac
     fi
 
-    # wrap around
+    # wrap selection
     if [ $SELECTED -lt 0 ]; then
         SELECTED=$((${#OPTIONS[@]} - 1))
     fi
+
     if [ $SELECTED -ge ${#OPTIONS[@]} ]; then
         SELECTED=0
     fi
